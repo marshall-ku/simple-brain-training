fd3545cce5dbee3dde809c823ea77a7f6bb1cf29
Marshall Ku <marshall@kakao.com>
2020-06-18 18:34:07 +0900

    Updates

---
+++ precache-manifest.e52b4282300cbf47774a037655aa01a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fd5eea55cab1ec3fce21778109c36c9",
+    "url": "/simple-brain-training/index.html"
+  },
+  {
+    "revision": "7fcd048c3e3c2baac7ba",
+    "url": "/simple-brain-training/static/css/main.a57652f8.chunk.css"
+  },
+  {
+    "revision": "885da52b948c7ec2f9f2",
+    "url": "/simple-brain-training/static/js/2.a99220b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-brain-training/static/js/2.a99220b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fcd048c3e3c2baac7ba",
+    "url": "/simple-brain-training/static/js/main.60dc5f09.chunk.js"
+  },
+  {
+    "revision": "4c5ef06f2274054493db",
+    "url": "/simple-brain-training/static/js/runtime-main.098ef1ce.js"
+  }
+]);
